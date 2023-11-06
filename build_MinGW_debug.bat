9408fa006aaea2c2ae9eadbcbaac35acbd64204b
OlehDvorychanskyi <oleh.dvorychanskyi@gmail.com>
2023-11-07 00:59:26 +0200

    Add multithreaded build option

--- build_MinGW_debug.bat
+++ build_MinGW_debug.bat
@@ -1,3 +1,3 @@
 cmake -S . -B build/MinGW_Debug -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
-cmake -E time cmake --build build/MinGW_Debug
+cmake -E time cmake --build build/MinGW_Debug -j 6
 PAUSE
--- build_MinGW_release.bat
+++ build_MinGW_release.bat
@@ -1,3 +1,3 @@
 cmake -S . -B build/MinGW_Release -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
-cmake --build build/MinGW_Release
+cmake -E time cmake --build build/MinGW_Release -j 6
 PAUSE
