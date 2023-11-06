86abf693c5a23ba39f849b85f6ea4ed96b6b5bd6
OlehDvorychanskyi <oleh.dvorychanskyi@gmail.com>
2023-11-06 21:05:50 +0200

    Add events, key and mouse codes

---
+++ Raccoon/src/Raccoon/Events/Event.h
@@ -0,0 +1,63 @@
+#pragma once
+
+namespace Raccoon
+{
+    enum class EventType
+    {
+        None = 0,
+        WindowResize, WindowClose,
+        KeyPressed, KeyReleased, KeyTyped,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+    };
+
+    enum EventCategory : uint32_t
+    {
+        None = 0,
+        WindowEvent = BIT(0),
+        KeyEvent = BIT(1),
+        MouseEvent = BIT(2)
+    };
+
+    #define DECLARE_EVENT_TYPE(type) static EventType GetStaticEventType() { return type; } virtual EventType GetEventType() const override { return type; }
+    #define DECLARE_EVENT_CATEGORY(category) virtual uint32_t GetEventCategory() const override { return category; } virtual bool BelongToCategory(EventCategory Category) const override { return category & Category; } 
+ 
+    #define RE_DEBUG
+        #define DECLARE_EVENT_DEBUG() virtual const char* GetEventName() const = 0; virtual std::string ToString() const = 0;
+        #define OVERRIDE_EVENT_DEBUG(name) virtual const char* GetEventName() const override { return name; }  virtual std::string ToString() const override { return name; }
+    #else
+        #define DECLARE_EVENT_DEBUG()
+        #define OVERRIDE_EVENT_DEBUG(name)
+    #endif
+
+    class Event
+    {
+        friend inline bool operator==(const std::shared_ptr<Event> &a, const std::shared_ptr<Event> &b) { return a->GetEventType() == b->GetEventType(); } 
+    public:
+        virtual EventType GetEventType() = 0;
+        virtual uint32_t GetEventCategory() = 0;
+        virtual bool BelongToCategory(EventCategory category) const = 0;
+        virtual ~Event() = default;
+
+        DECLARE_EVENT_DEBUG()
+    public:
+        bool Handled = false;
+    };
+
+    class EventDispacher
+    {
+    public:
+        EventDispacher(Event &event)
+            : m_Event(event) {}
+        
+        template <typename DynamicEventType, typename CallbackType>
+        void Dispatch(const CallbackType &callback)
+        {
+            if (m_Event.GetEventType() == DynamicEventType::GetStaticEventType())
+            {
+                callback(static_cast<DynamicEventType&>(m_Event));
+            }
+        }
+    private:
+        Event &m_Event;
+    };
+}
---
+++ Raccoon/src/Raccoon/Events/MouseEvents.h
@@ -0,0 +1,75 @@
+#pragma once 
+#include <Raccoon/Events/Event.h>
+#include <Raccoon/Core/MouseCodes.h>
+
+namespace Raccoon
+{
+    class MouseMovedEvent : public Event
+    {
+    public:
+        MouseMovedEvent(float x, float y)
+            : m_X{x}, m_Y{y} {}
+
+        float GetX() const { return m_X; }
+        float GetY() const { return m_Y; }
+
+        DECLARE_EVENT_TYPE(EventType::MouseMoved);
+        DECLARE_EVENT_CATEGORY(EventCategory::MouseEvent);
+        DECLARE_EVENT_DEBUG("MouseMovedEvent");
+    private:
+        float m_X, m_Y;
+    };
+
+    class MouseScrolledEvent : public Event
+    {
+    public:
+        MouseScrolledEvent(float XOffset, float YOffset)
+            : m_XOffset{XOffset}, m_YOffset{YOffset} { } 
+
+        float GetXOffset() const { return m_XOffset; }
+        float GetYOffset() const { return m_YOffset; }
+
+        DECLARE_EVENT_TYPE(EventType::MouseScrolled);
+        DECLARE_EVENT_CATEGORY(EventCategory::MouseEvent);
+        DECLARE_EVENT_DEBUG("MouseScrolledEvent");
+    private:
+        float m_XOffset, m_YOffset;
+    };
+
+    class MouseButtonEvent : public Event
+    {
+    public:
+        MouseCode GetMouseCode() const { return m_MouseCode; }
+
+        DECLARE_EVENT_TYPE(EventType::MouseButtonEvent);
+        DECLARE_EVENT_CATEGORY(EventCategory::MouseEvent);
+        DECLARE_EVENT_DEBUG("MouseButtonEvent");
+    protected:
+        MouseButtonEvent(MouseCode mousecode)
+            : m_MouseCode{mousecode} { }
+    protected:
+        MouseCode m_MouseCode;
+    };
+
+    class MouseButtonPressedEvent : public MouseButtonEvent
+    {
+    public:
+        MouseButtonPressedEvent(MouseCode mousecode)
+            : MouseButtonEvent(mousecode) { }
+
+        DECLARE_EVENT_TYPE(EventType::MouseButtonEvent);
+        DECLARE_EVENT_CATEGORY(EventCategory::MouseEvent);
+        DECLARE_EVENT_DEBUG("MouseButtonPressedEvent");
+    };
+
+    class MouseButtonReleasedEvent : public MouseButtonEvent
+    {
+    public:
+        MouseButtonReleasedEvent(MouseCode mousecode)
+            : MouseButtonEvent(mousecode) { }   
+
+        DECLARE_EVENT_TYPE(EventType::MouseButtonEvent);
+        DECLARE_EVENT_CATEGORY(EventCategory::MouseEvent);
+        DECLARE_EVENT_DEBUG("MouseButtonReleasedEvent");
+    };
+}
